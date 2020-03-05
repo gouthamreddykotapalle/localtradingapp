39688d58ff2d15558485c0be5457550687685207
Nathan Smalley <nathan@smalley.io>
2020-03-04 22:24:31 -0500

    add controller for static html pages and adjust routes appropriately

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get '/', to: 'static_page#home'
+  get '/about', to: 'static_page#about'
+  get '/contact', to: 'static_page#contact'
+  get '/privacypolicy', to: 'static_page#privacypolicy'
+  get '/terms', to: 'static_page#terms'
+
+  ###
+
+  get 'static_page/home'
+  get 'static_page/about'
+  get 'static_page/contact'
+  get 'static_page/privacypolicy'
+  get 'static_page/terms'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
