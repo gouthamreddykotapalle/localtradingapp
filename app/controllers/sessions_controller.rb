8753465aefeda9c297ff3c49133a431d889bc899
Mingen Pan <panmingen@uchicago.edu>
2020-03-06 01:46:32 -0500

    merge Mingen's login code with Goutham's Add logout logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,13 @@ class SessionsController < ApplicationController
     login_info = get_post_object :login_info
     email = login_info.require(:email)
     password = login_info.require :password
-    user = User.authenticate email, password
+    begin
+      user = User.authenticate email, password
+    rescue => error
+      flash[:notice] = error.message.to_s
+      redirect_to '/login'
+      return
+    end
     session[:user_id] = user.email
     redirect_back fallback_location: '/'
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,9 @@ class User < ApplicationRecord
   def self.authenticate(email, password)
     user = User.find_by_email email
     if user == nil
-      flash[:notice] = "email does not exist"
-      redirect_to "/login"
+      raise ArgumentError.new "email does not exist"
     elsif user.password != password
-      flash[:notice] = "password is incorrect"
-      redirect_to "/login"
+      raise ArgumentError.new "password is incorrect"
     else
       return user
     end
