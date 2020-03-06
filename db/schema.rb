297f649d93bb7b7e50324a4ab528ed2d781b8324
Nathan Smalley <nathan@smalley.io>
2020-03-05 22:32:38 -0500

    add cucumber

--- db/schema.rb
+++ db/schema.rb
@@ -35,15 +35,14 @@ ActiveRecord::Schema.define(version: 2020_03_05_154724) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", limit: 40
-    t.string "password", limit: 512
     t.string "first_name"
     t.string "last_name"
-    t.datetime "last_updated"
+    t.string "email"
+    t.string "password"
     t.integer "active_posts"
+    t.datetime "created"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
