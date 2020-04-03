6197995723ae575ea5ca9f24a7898c2e84493f5c
Nathan Smalley <nathan@smalley.io>
2020-04-03 17:30:07 -0400

    simple list view for map markers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,60 +17,72 @@
 //= require turbolinks
 //= require_tree .
 
-let locations = [
-    // {lat: 40.7128, lng: -74.0060},
-    // {lat: 44.971730, lng: -93.217200},
-    // {lat:44.971217, lng:-93.214503},
-    // {lat: 44.973251, lng: -93.216284},
-    // {lat: 44.976029, lng: -93.224546}
-];
+var locations, markers = [];
 
-function getAllSalePosts() {
+function populateMarkers(map, sell_posts) {
+    for (let i = 0; i < sell_posts.length; i++) {
+        let latlong = {lat: sell_posts[i].latitude, lng: sell_posts[i].longitude};
+
+        locations.push(latlong);
+        markers.push(new google.maps.Marker({position: latlong, map: map}))
+    }
+    // let marker = new google.maps.Marker({position: locations[0], map: map});
+    console.log(locations, markers);
+}
+
+function init_markers(map) {
     console.log('getAllSalePosts() called');
     Rails.ajax({
         type: "GET",
         url: '/sell_posts/map/all', // TODO: proper api conventions
         data: '',
         success: function(data) {
-            // let sell_posts = [];
-            //
-            // for (let i = 0; i < data.length; i++) {
-            //     sell_posts.push(JSON.parse(data[i]))
-            // }
-
             console.log('success for getting all sell posts ' + data);
 
-            Rails.$("#sellposts-maplist").html = data;
+            populateSellPostsList(data);
+            populateMarkers(map, data);
         }
     })
 }
 
-$(document).ready(function () {
+function populateSellPostsList(sell_posts) {
+    console.log('populating sell posts list');
 
-    function initMap(element, lat, lng) {
+    $("#sellposts-maplist").empty();
 
-        let myCoords = new google.maps.LatLng(lat, lng);
+    $("#sellposts-maplist").append(`<ul>`);
 
-        let mapOptions = {
-            center: myCoords,
-            zoom: 8
-        };
-
-        let map = new google.maps.Map(element, mapOptions);
-
-        // generate markers here
-
-
-
-        let marker = new google.maps.Marker({position: locations[0], map: map});
-
+    for (let i = 0; i < sell_posts.length; i++) {
+        $("#sellposts-maplist").append(`
+            <li>${sell_posts[i].title}, ${sell_posts[i].category}, Price: ${sell_posts[i].price}, Bargain? ${sell_posts[i].bargain_allowed}, Location: ${sell_posts[i].latitude} ${sell_posts[i].longitude}</li>
+        `);
     }
 
+    $("#sellposts-maplist").append(`</ul>`);
+}
+
+function initMap(element, lat, lng) {
+
+    let myCoords = new google.maps.LatLng(lat, lng);
+
+    let mapOptions = {
+        center: myCoords,
+        zoom: 8
+    };
+
+    return map = new google.maps.Map(element, mapOptions);
+
+    // let marker = new google.maps.Marker({position: locations[0], map: map});
+
+}
+
+$(document).ready(function () {
     console.log('initializing map');
-    initMap(document.getElementById('gmap'), 40.7128,-74.0060);
+    let map = initMap(document.getElementById('gmap'), 40.7128,-74.0060);
     console.log('map initialized');
 
-    getAllSalePosts();
+    init_markers(map);
+
 });
 //
 // $(document).ready(function() {
